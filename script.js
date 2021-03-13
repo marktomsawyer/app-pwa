90df296a91a11e8f1d7fa836850850b5ec39f7ab
Mark Tom Sawyer <mark@tomsawyer.com.br>
2021-03-13 17:58:20 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,14 +2,14 @@
 if ("serviceWorker" in navigator) {
     window.addEventListener("load", function () {
         navigator.serviceWorker
-            .register("/serviceWorker.js")
+            .register("serviceWorker.js")
             .then(res => console.log("service worker registered"))
             .catch(err => console.log("service worker not registered", err))
     })
 }
 
 function calc() {
-    
+
 
     let n1 = document.getElementById('n1');
     let n2 = document.getElementById('n2');
