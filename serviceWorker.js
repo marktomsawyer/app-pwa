bc32e14c66def270024a0627955de9ab410614d5
Mark Tom Sawyer <mark@tomsawyer.com.br>
2021-03-13 17:33:37 -0300

    Create serviceWorker.js

---
+++ serviceWorker.js
@@ -0,0 +1,22 @@
+const detomiDev = "calc-calender-v1"
+const assets = [
+    "/",
+    "/index.html",
+    "/scripts.js",
+]
+
+self.addEventListener("install", installEvent => {
+    installEvent.waitUntil(
+        caches.open(detomiDev).then(cache => {
+            cache.addAll(assets)
+        })
+    )
+})
+
+self.addEventListener("fetch", fetchEvent => {
+    fetchEvent.respondWith(
+        caches.match(fetchEvent.request).then(res => {
+            return res || fetch(fetchEvent.request)
+        })
+    )
+})
