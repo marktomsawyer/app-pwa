511b9c9753f53f7287be61145e07e2e1fee67598
Mark Tom Sawyer <mark@tomsawyer.com.br>
2021-03-13 17:33:31 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,25 @@
+
+if ("serviceWorker" in navigator) {
+    window.addEventListener("load", function () {
+        navigator.serviceWorker
+            .register("/serviceWorker.js")
+            .then(res => console.log("service worker registered"))
+            .catch(err => console.log("service worker not registered", err))
+    })
+}
+
+function calc() {
+
+    let n1 = document.getElementById('n1');
+    let n2 = document.getElementById('n2');
+    let resultado = document.getElementById('resultado');
+
+    console.log(n1.value);
+    console.log(n2.value);
+
+
+    resultado.value = Number(n1.value) + Number(n2.value);
+
+    console.log(resultado.value);
+
+}
